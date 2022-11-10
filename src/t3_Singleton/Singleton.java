3f957f0d530373e0b87ed1aeebbbaa09ecb0e9bd
damjoz1990 <damjoz1990@gmail.com>
2022-11-10 13:00:05 +0100

    Singleton, How to make only one object

---
+++ src/t3_Singleton/Singleton.java
@@ -0,0 +1,11 @@
+package t3_Singleton;
+
+public class Singleton {
+        private static final Singleton instance = new Singleton();
+
+        public static Singleton getInstance() {
+            return instance;
+        }
+        private Singleton() {
+        }
+}
