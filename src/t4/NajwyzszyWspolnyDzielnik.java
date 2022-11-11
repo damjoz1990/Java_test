135d7fdd143f63fe3206e6221c395e6816379b8c
damjoz1990 <damjoz1990@gmail.com>
2022-11-11 19:53:29 +0100

    The highest common divider

---
+++ src/t4/NajwyzszyWspolnyDzielnik.java
@@ -0,0 +1,33 @@
+package t4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NajwyzszyWspolnyDzielnik {
+
+        public static void main(String[] args) throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            int first = Integer.parseInt(reader.readLine());
+            int second = Integer.parseInt(reader.readLine());
+
+            System.out.println(getNOD(first, second));
+        }
+
+        private static int getNOD(int first, int second) {
+            if(first < 1 || second < 1) {
+                throw new IllegalArgumentException();
+            }
+
+            while (first != second) {
+                if (first > second) {
+                    first -= second;
+                }
+                if (second > first) {
+                    second -= first;
+                }
+            }
+            return first;
+        }
+
+}
